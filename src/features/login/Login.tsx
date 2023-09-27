a3c43dce410d07072abf183ac863f084c5e52c4e
efisher5 <ecfisher13@gmail.com>
2023-09-27 17:56:36 -0400

    converted login styles to css modules

--- src/features/login/Login.tsx
+++ src/features/login/Login.tsx
@@ -2,7 +2,7 @@ import { Formik, Form } from 'formik';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMortarPestle } from '@fortawesome/free-solid-svg-icons';
 import { useAuth0 } from '@auth0/auth0-react';
-import './Login.css';
+import loginStyles from './Login.module.css';
 
 interface LoginValues {
     username: string;
@@ -18,18 +18,18 @@ export default function LoginForm() {
     }
 
     return (
-        <div className='login-form'>
-            <div className='login-wrapper'>
-                <div className='icon-wrapper'>
+        <div className={loginStyles.loginForm}>
+            <div className={loginStyles.loginWrapper}>
+                <div className={loginStyles.iconWrapper}>
                     <FontAwesomeIcon icon={faMortarPestle} size='2x' className='secondary-color' />
                 </div>
-                <h2 className='login-title'>Recipease</h2>
+                <h2 className={loginStyles.loginTitle}>Recipease</h2>
                 <div>Let's get cookin'</div>
                 <Formik initialValues={initialValues} onSubmit={() => loginWithRedirect()}>
                     {() => (
                         <Form>
-                            <div className='login-btn-wrapper'>
-                                <button className='login-btn' type="submit">Login</button>
+                            <div className={loginStyles.loginBtnWrapper}>
+                                <button className={loginStyles.loginBtn} type="submit">Login</button>
                             </div>
                         </Form>
                     )}
